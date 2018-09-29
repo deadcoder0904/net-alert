2d9f08dfdf168b8209d1baba2ad353a4a1f1f5df
Akshay Kadam <akshaykadam0904@gmail.com>
2018-09-29 11:02:52 +0530

    Improve README ✨

--- Net Alert/AppDelegate.swift
+++ Net Alert/AppDelegate.swift
@@ -4,59 +4,59 @@ import Defaults
 import LaunchAtLogin
 
 extension Defaults.Keys {
-    static let launchAtLogin = Defaults.Key<Bool>("launchAtLogin", default: false)
+	static let launchAtLogin = Defaults.Key<Bool>("launchAtLogin", default: false)
 }
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 	
-		let REMOTE_URL: String = "http://captive.apple.com"
+	let REMOTE_URL: String = "http://captive.apple.com"
 	
-		var launchAtLoginMenuItem = NSMenuItem()
-    
-    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+	var launchAtLoginMenuItem = NSMenuItem()
+	
+	let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
 
-    let reachability = Reachability()!
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        constructMenu()
-    }
+	let reachability = Reachability()!
+	
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		constructMenu()
+	}
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-    
-    func constructMenu() {
-        if let button = statusItem.button {
-            button.image = NSImage(named: NSImage.Name("green"))
-        }
-        
-        let menu = NSMenu()
-				launchAtLoginMenuItem = NSMenuItem(title: "Launch At Login", action: #selector(self.toggleLaunchAtLogin), keyEquivalent: "")
-				launchAtLoginMenuItem.state = defaults[.launchAtLogin] ? .on : .off
-        menu.addItem(launchAtLoginMenuItem)
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
-        
-        statusItem.menu = menu
-        checkConnection()
-    }
-    
-    @objc func toggleLaunchAtLogin() {
-        let launchAtLogin = defaults[.launchAtLogin]
-        launchAtLoginMenuItem.title = "Launch At Login"
-				launchAtLoginMenuItem.state = !launchAtLogin ? .on : .off
-        LaunchAtLogin.isEnabled = !launchAtLogin
-        defaults[.launchAtLogin] = !launchAtLogin
-    }
-    
-    func setStatusIcon(color: String) {
-        if let button = self.statusItem.button {
-            if button.image?.name() != color {
-                button.image = NSImage(named: NSImage.Name(color))
-            }
-        }
-    }
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+	
+	func constructMenu() {
+		if let button = statusItem.button {
+			button.image = NSImage(named: NSImage.Name("green"))
+		}
+		
+		let menu = NSMenu()
+		launchAtLoginMenuItem = NSMenuItem(title: "Launch At Login", action: #selector(self.toggleLaunchAtLogin), keyEquivalent: "")
+		launchAtLoginMenuItem.state = defaults[.launchAtLogin] ? .on : .off
+		menu.addItem(launchAtLoginMenuItem)
+		menu.addItem(NSMenuItem.separator())
+		menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+		
+		statusItem.menu = menu
+		checkConnection()
+	}
+	
+	@objc func toggleLaunchAtLogin() {
+		let launchAtLogin = defaults[.launchAtLogin]
+		launchAtLoginMenuItem.title = "Launch At Login"
+		launchAtLoginMenuItem.state = !launchAtLogin ? .on : .off
+		LaunchAtLogin.isEnabled = !launchAtLogin
+		defaults[.launchAtLogin] = !launchAtLogin
+	}
+	
+	func setStatusIcon(color: String) {
+		if let button = self.statusItem.button {
+			if button.image?.name() != color {
+				button.image = NSImage(named: NSImage.Name(color))
+			}
+		}
+	}
 	
 	func pingHost(_ fullURL: String) {
 
@@ -90,29 +90,32 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 				}
 				.resume()
 		}
+		else {
+			 self.setStatusIcon(color: "yellow")
+		}
 	}
 
-    func checkConnection() {
-        reachability.whenReachable = { reachability in
-            if reachability.connection == .wifi {
-                print("Reachable via WiFi")
-								self.pingHost(self.REMOTE_URL)
-            } else {
-                print("Reachable via Cellular")
-								self.pingHost(self.REMOTE_URL)
-            }
-        }
-        
-        reachability.whenUnreachable = { _ in
-            print("Not reachable")
-            self.setStatusIcon(color: "red")
-        }
-        
-        do {
-            try reachability.startNotifier()
-        } catch {
-            print("Unable to start notifier")
-            self.setStatusIcon(color: "red")
-        }
-    }
+	func checkConnection() {
+		reachability.whenReachable = { reachability in
+			if reachability.connection == .wifi {
+				print("Reachable via WiFi")
+				self.pingHost(self.REMOTE_URL)
+			} else {
+				print("Reachable via Cellular")
+				self.pingHost(self.REMOTE_URL)
+			}
+		}
+		
+		reachability.whenUnreachable = { _ in
+			print("Not reachable")
+			self.setStatusIcon(color: "red")
+		}
+		
+		do {
+			try reachability.startNotifier()
+		} catch {
+			print("Unable to start notifier")
+			self.setStatusIcon(color: "yellow")
+		}
+	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # net-alert
+
+> A menu bar app to check if Internet is ✅ Online or ❌ Offline
+
+It is written in Swift. This is my 2nd project made for fun when my internet become shitty.
+
+Initially I used [is-online](https://github.com/sindresorhus/is-online) by [@sindresorhus](https://github.com/sindresorhus) but my internet sucked too much & it was constantly going offline & online.
+
+So I made this little app which will acts like traffic signal for the internet connection.
+
+It will be Green if Internet is Online, Red if its Offline & Yellow if there is an Error while connecting.
+
+If you like this app, checkout my 1st app also made in Swift called [Away From Keyboard (AFK)](https://akshaykadam.me/apps/away-from-keyboard/?ref=github). AFK is for anyone who works for a long time on a computer to reduce eye strain by getting reminded by AFK to take a break.

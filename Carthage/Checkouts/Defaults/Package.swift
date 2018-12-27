27007927551826f95131d46adb5520e322a7abde
Akshay Kadam <akshaykadam0904@gmail.com>
2018-12-27 09:34:18 +0530

    Updated Deps via Carthage

--- Carthage/Checkouts/Defaults/Package.swift
+++ Carthage/Checkouts/Defaults/Package.swift
@@ -1,21 +1,25 @@
-// swift-tools-version:4.1
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
-    name: "Defaults",
-    products: [
-        .library(
-            name: "Defaults",
-            targets: ["Defaults"]
+	name: "Defaults",
+	products: [
+		.library(
+			name: "Defaults",
+			targets: [
+				"Defaults"
+			]
 		)
-    ],
-    targets: [
-        .target(
-            name: "Defaults"
+	],
+	targets: [
+		.target(
+			name: "Defaults"
 		),
-        .testTarget(
-            name: "DefaultsTests",
-            dependencies: ["Defaults"]
+		.testTarget(
+			name: "DefaultsTests",
+			dependencies: [
+				"Defaults"
+			]
 		)
-    ]
+	]
 )
